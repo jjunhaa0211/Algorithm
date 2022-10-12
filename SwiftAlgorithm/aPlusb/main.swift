dedeb5de0c154f58951ed3bc08bc02f93c93258b
junha <goodjunha@gmail.com>
2022-10-12 11:58:57 +0900

    🐛:: 백준 1000번 A+B

---
+++ SwiftAlgorithm/aPlusb/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  aPlusb
+//
+//  Created by 박준하 on 2022/10/12.
+//
+
+import Foundation
+
+let c = readLine()!
+let cArr = c.components(separatedBy: " ")
+let a = Int(cArr[0])!
+let b = Int(cArr[1])!
+
+print(a + b)
