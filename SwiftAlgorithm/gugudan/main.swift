e3def997547f01011f38076654eac117b751a11f
junha <goodjunha@gmail.com>
2022-10-13 16:31:26 +0900

    💩:: 백준 2739 구구단

---
+++ SwiftAlgorithm/gugudan/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  gugudan
+//
+//  Created by 박준하 on 2022/10/13.
+//
+
+import Foundation
+
+var a = Int(readLine()!)
+
+for i in 1...9 {
+    print("\(a!) * \(i) = \(a! * i)")
+}
