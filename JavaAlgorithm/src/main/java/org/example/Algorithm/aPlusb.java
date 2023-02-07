d7aeeb6c8267c24a49594ccb4c5eff41a488c34f
junha <goodjunha@gmail.com>
2023-02-07 19:35:40 +0900

    🌈:: 백준 1000번 A+B

---
+++ JavaAlgorithm/src/main/java/org/example/Algorithm/aPlusb.java
@@ -0,0 +1,13 @@
+package org.example.Algorithm;
+
+import java.util.Scanner;
+
+public class aPlusb {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+    }
+}
