d8f5c19fd234f6ee345a3f2168ef1fc3573a6175
junha <goodjunha@gmail.com>
2023-02-07 19:28:35 +0900

    🌪:: aMinuseb ️

---
+++ JavaAlgorithm/src/main/java/org/example/Algorithm/aMinusb.java
@@ -0,0 +1,16 @@
+package org.example.Algorithm;
+
+import java.util.Scanner;
+
+public class aMinusb {
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a-b);
+    }
+}
