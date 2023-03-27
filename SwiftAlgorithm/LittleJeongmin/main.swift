4a799f87f9fa6a8267978f1505568cf93d0ced13
junha <goodjunha@gmail.com>
2023-03-27 12:15:54 +0900

    🤥:: 백준 11382번 꼬마 정민(swift)

---
+++ SwiftAlgorithm/LittleJeongmin/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  LittleJeongmin
+//
+//  Created by 박준하 on 2023/03/27.
+//
+
+import Foundation
+
+let cArr = readLine()!.components(separatedBy: " ")
+let a = Int(cArr[0])!
+let b = Int(cArr[1])!
+let c = Int(cArr[2])!
+
+print(a + b + c)
