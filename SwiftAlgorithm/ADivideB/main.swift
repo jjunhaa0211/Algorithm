ee119902803cb3cbe4dc91fb19a742feae2eff4d
junha <goodjunha@gmail.com>
2023-03-27 11:40:51 +0900

    🤫:: 백준 10818번 A/B

---
+++ SwiftAlgorithm/ADivideB/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  ADivideB
+//
+//  Created by 박준하 on 2023/03/27.
+//
+
+import Foundation
+
+let cArr = readLine()!.components(separatedBy: " ")
+let a = Double(cArr[0])!
+let b = Double(cArr[1])!
+
+print(a / b)
