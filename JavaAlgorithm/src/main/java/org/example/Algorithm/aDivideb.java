e99361e11636e8f3b5fe884c0a4172b54891fbbf
junha <goodjunha@gmail.com>
2023-03-27 11:46:38 +0900

    🤫:: 백준 10818번 A/B

---
+++ JavaAlgorithm/src/main/java/org/example/Algorithm/aDivideb.java
@@ -0,0 +1,16 @@
+package org.example.Algorithm;
+
+import java.util.Scanner;
+
+public class aDivideb {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Double a;
+        Double b;
+
+        a = input.nextDouble();
+        b = input.nextDouble();
+
+        System.out.println(a / b);
+    }
+}
