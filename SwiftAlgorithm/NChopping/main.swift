88bea53f9c17c345918cafcf06866351e1fac3b7
junha <goodjunha@gmail.com>
2023-03-28 10:19:14 +0900

    🧓:: 백준 2741번 N 찍기 (swift)

---
+++ SwiftAlgorithm/NChopping/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  NChopping
+//
+//  Created by 박준하 on 2023/03/28.
+//
+
+import Foundation
+
+let cArr = readLine()!.components(separatedBy: " ")
+var a = Int(cArr[0])!
+
+for i in 1...a {
+    print(i)
+}
