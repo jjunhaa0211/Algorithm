631782a7f84819461124464ba829d9c3a339f080
junha <goodjunha@gmail.com>
2023-03-28 09:59:23 +0900

    🕵️:: 백준 7282번 새싹 (java)

---
+++ JavaAlgorithm/src/main/java/org/example/Algorithm/Registration.java
@@ -0,0 +1,8 @@
+package org.example.Algorithm;
+
+public class Registration {
+    public static void main(String[] args) {
+        System.out.println("26");
+        System.out.print("junha0211");
+    }
+}
