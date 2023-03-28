cac5bd3ba526a65a25c1bba9f5fea6623bd92824
junha <goodjunha@gmail.com>
2023-03-28 09:46:37 +0900

    👶:: 백준 25083번 새싹 (swift)

---
+++ SwiftAlgorithm/Sprout/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Sprout
+//
+//  Created by 박준하 on 2023/03/28.
+//
+
+import Foundation
+
+let result = #"""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+"""#
+
+print(result)
