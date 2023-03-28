b5130e2bc9a45cca7cc2c554fdb9a65c3bb84542
junha <goodjunha@gmail.com>
2023-03-28 09:53:49 +0900

    👶:: 백준 25083번 새싹 (java)

---
+++ JavaAlgorithm/src/main/java/org/example/Algorithm/Sprout.java
@@ -0,0 +1,18 @@
+package org.example.Algorithm;
+
+public class Sprout {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("      |");
+        System.out.print("      |");
+    }
+}
+
+//         ,r'"7
+//r`-_   ,'  ,/
+// \. ". L_r'
+//   `~\/
+//      |
+//      |
