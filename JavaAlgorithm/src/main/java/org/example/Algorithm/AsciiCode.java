fda6a29e2874d4ecd372675c90092c1120ab101e
junha <goodjunha@gmail.com>
2023-04-01 19:57:25 +0900

    😉:: 백준 11654번 아스키 코드(java)

---
+++ JavaAlgorithm/src/main/java/org/example/Algorithm/AsciiCode.java
@@ -0,0 +1,13 @@
+package org.example.Algorithm;
+
+import java.util.Scanner;
+
+public class AsciiCode {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.next().charAt(0);
+
+        System.out.println(n);
+    }
+}
