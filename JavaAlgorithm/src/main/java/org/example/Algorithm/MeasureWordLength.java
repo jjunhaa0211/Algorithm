bfbbf2227beabed8c76d628752350e7b64d92c28
junha <goodjunha@gmail.com>
2023-04-04 08:19:03 +0900

    ✊🏿:: 백준 2743번 단어 길이 재기 (java)

---
+++ JavaAlgorithm/src/main/java/org/example/Algorithm/MeasureWordLength.java
@@ -0,0 +1,13 @@
+package org.example.Algorithm;
+
+import java.util.Scanner;
+
+public class MeasureWordLength {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String data = input.next();
+
+        System.out.println(data.length());
+    }
+}
