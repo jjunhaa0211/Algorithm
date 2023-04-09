d7a9a4ab8b7c48599f491fd995f2da9a8e6a9b63
junha <goodjunha@gmail.com>
2023-04-10 08:29:59 +0900

    🙃:: 백준  5338번 마이크로소프트 로고 (java)

---
+++ JavaAlgorithm/src/main/java/org/example/Algorithm/MicrosoftLogo.java
@@ -0,0 +1,11 @@
+package org.example.Algorithm;
+
+public class MicrosoftLogo {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.print("'-..-'|_.-''-._|");
+    }
+}
