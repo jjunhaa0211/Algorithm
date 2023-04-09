63a51ea1fd3622572b181ec20acd4184a238d79e
junha <goodjunha@gmail.com>
2023-04-10 08:25:55 +0900

    🙃:: 백준  5338번 마이크로소프트 로고 (swift)

---
+++ SwiftAlgorithm/MicrosoftLogo/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  MicrosoftLogo
+//
+//  Created by 박준하 on 2023/04/10.
+//
+
+import Foundation
+
+let microsoftLogo = #"""
+       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+"""#
+
+print(microsoftLogo)
